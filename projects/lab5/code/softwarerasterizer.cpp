170e0d5c1c234c92ecaa0b52bb4d747b8c3f709d
Luddw <ludlin-8@student.ltu.se>
2020-07-30 03:01:52 +0200

    started on framebuffer

--- projects/lab5/code/softwarerasterizer.cpp
+++ projects/lab5/code/softwarerasterizer.cpp
@@ -1,10 +1,33 @@
 #include "softwarerasterizer.h"
 
 
+
+
 Renderer::Renderer()
 {
 }
 
 Renderer::~Renderer()
+{
+}
+
+void Renderer::AddVertexBuffer(Vertex * buffer) 
+{
+	
+}
+
+void Renderer::AddIndexBuffer(unsigned int * buffer)
+{
+	
+}
+
+const unsigned int Renderer::AddBuffer(std::vector<Vertex> vbuff, std::vector<unsigned int> ibuff)
+{
+	unsigned int t_handle = buffer_handles.size() + 1;
+	buffer_handles.emplace(t_handle, BufferObject(vbuff, ibuff));
+	return t_handle;
+}
+
+void Renderer::SetupFrameBuffer()
 {
 }
--- projects/lab5/code/softwarerasterizer.h
+++ projects/lab5/code/softwarerasterizer.h
@@ -1,14 +1,43 @@
 #pragma once
 #include <GL/glew.h>
+#include <map>
+#include <iterator>
+#include "matlib.h"
+#include <vector>
+
+struct Vertex
+{
+	Vector4D pos;
+	Vector4D color;
+	Vector4D normal;
+};
+
+struct BufferObject
+{
+	
+	std::vector<Vertex> v_buffer;
+	std::vector<unsigned int> i_buffer;
+
+	BufferObject(std::vector<Vertex> v_buff, std::vector<unsigned int> i_buff)
+	{
+		this->v_buffer = v_buff;
+		this->i_buffer = i_buff;
+	};
+};		
 
 class Renderer
 {
 public:
 	Renderer();
 	~Renderer();
-
+	void AddVertexBuffer(Vertex* buffer);
+	void AddIndexBuffer(unsigned int* buffer);
+	const unsigned int AddBuffer(std::vector<Vertex> vbuff, std::vector<unsigned int> ibuff);
+	void SetupFrameBuffer();
 private:
-	GLuint vb, ibo;
+	GLuint vbo{}, ibo{};
+	GLuint fbo;
+	std::map<unsigned int, BufferObject> buffer_handles;
 	unsigned int frame_buffer;
 };
 
